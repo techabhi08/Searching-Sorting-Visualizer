43e39df5392cf88312dccde630a48aa19a5dbc79
Anmol Verma <anmolverma102002@gmail.com>
2021-11-08 17:43:35 +0530

    Debugged and fixed not generating array issue

--- JS/Algorithms/merge_sort.js
+++ JS/Algorithms/merge_sort.js
@@ -1,13 +1,24 @@
-function Merge_sort()
+/*
+    *****************
+    DONE BY:-   TUMMALA KETHAN
+    E-MAIL:-    tummala.kethan@gmail.com
+    *****************
+*/
+
+function Merge()
 {
     c_delay=0;
-    mergeSort(0,array_size-1);
+    console.log(("Selected M"));
+
+    merge_partition(0,array_size-1);
+
     enable_buttons();
 }
 
-function merge(start,mid,end)
+function merge_sort(start,mid,end)
 {
     var p=start,q=mid+1;
+
     var Arr=[],k=0;
 
     for(var i=start; i<=end; i++)
@@ -15,42 +26,49 @@ function merge(start,mid,end)
         if(p>mid)
         {
             Arr[k++]=div_sizes[q++];
-            div_update(divs[q-1],div_sizes[q-1],"red");
+            div_update(divs[q-1],div_sizes[q-1],"red");//Color update
         }
         else if(q>end)
         {
             Arr[k++]=div_sizes[p++];
-            div_update(divs[p-1],div_sizes[p-1],"red");
+            div_update(divs[p-1],div_sizes[p-1],"red");//Color update
         }
         else if(div_sizes[p]<div_sizes[q])
         {
             Arr[k++]=div_sizes[p++];
-            div_update(divs[p-1],div_sizes[p-1],"red");
+            div_update(divs[p-1],div_sizes[p-1],"red");//Color update
         }
         else
         {
             Arr[k++]=div_sizes[q++];
-            div_update(divs[q-1],div_sizes[q-1],"red");
+            div_update(divs[q-1],div_sizes[q-1],"red");//Color update
         }
     }
 
     for(var t=0;t<k;t++)
     {
         div_sizes[start++]=Arr[t];
-        div_update(divs[start-1],div_sizes[start-1],"green");
+        div_update(divs[start-1],div_sizes[start-1],"green");//Color update
     }
 }
 
-function mergeSort(start,end)
+function merge_partition(start,end)
 {
     if(start < end)
     {
         var mid=Math.floor((start + end) / 2);
-        div_update(divs[mid],div_sizes[mid],"yellow");
+        div_update(divs[mid],div_sizes[mid],"yellow");//Color update
 
-        mergeSort(start,mid);
-        mergeSort(mid+1,end);
+        merge_partition(start,mid);
+        merge_partition(mid+1,end);
 
-        merge(start,mid,end);
+        merge_sort(start,mid,end);
     }
 }
+// export default Merge ;
+/*
+    *****************
+    DONE BY:-   TUMMALA KETHAN
+    E-MAIL:-    tummala.kethan@gmail.com
+    *****************
+*/
--- JS/Algorithms/selection_sort.js
+++ JS/Algorithms/selection_sort.js
@@ -1,29 +1,36 @@
+/*
+    *****************
+    DONE BY:-   TUMMALA KETHAN
+    E-MAIL:-    tummala.kethan@gmail.com
+    *****************
+*/
+
 function Selection_sort()
 {
     c_delay=0;
 
     for(var i=0;i<array_size-1;i++)
     {
-        div_update(divs[i],div_sizes[i],"red");
+        div_update(divs[i],div_sizes[i],"red");//Color update
 
         index_min=i;
 
         for(var j=i+1;j<array_size;j++)
         {
-            div_update(divs[j],div_sizes[j],"yellow");
+            div_update(divs[j],div_sizes[j],"yellow");//Color update
 
             if(div_sizes[j]<div_sizes[index_min])
             {
                 if(index_min!=i)
                 {
-                    div_update(divs[index_min],div_sizes[index_min],"blue");
+                    div_update(divs[index_min],div_sizes[index_min],"blue");//Color update
                 }
                 index_min=j;
-                div_update(divs[index_min],div_sizes[index_min],"red");
+                div_update(divs[index_min],div_sizes[index_min],"red");//Color update
             }
             else
             {
-                div_update(divs[j],div_sizes[j],"blue");
+                div_update(divs[j],div_sizes[j],"blue");//Color update
             }
         }
         
@@ -33,13 +40,20 @@ function Selection_sort()
             div_sizes[index_min]=div_sizes[i];
             div_sizes[i]=temp;
 
-            div_update(divs[index_min],div_sizes[index_min],"red");
-            div_update(divs[i],div_sizes[i],"red");
-            div_update(divs[index_min],div_sizes[index_min],"blue");
+            div_update(divs[index_min],div_sizes[index_min],"red");//Height update
+            div_update(divs[i],div_sizes[i],"red");//Height update
+            div_update(divs[index_min],div_sizes[index_min],"blue");//Color update
         }
-        div_update(divs[i],div_sizes[i],"green");
+        div_update(divs[i],div_sizes[i],"green");//Color update
     }
-    div_update(divs[i],div_sizes[i],"green");
+    div_update(divs[i],div_sizes[i],"green");//Color update
 
     enable_buttons();
 }
+
+/*
+    *****************
+    DONE BY:-   TUMMALA KETHAN
+    E-MAIL:-    tummala.kethan@gmail.com
+    *****************
+*/
